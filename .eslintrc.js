fe744ed66384314244a4dc4a513d2c62771e38d1
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-06-20 21:42:42 +0200

    Apply eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "experimentalObjectRestSpread": true
+      }
+  },
+  "rules": {
+      "no-console": ["error", { "allow": ["log", "warn", "error"] }],
+      "indent": [ "error", 2 ],
+      "linebreak-style": [ "error", "unix" ],
+      "quotes": [ "error", "single", { "allowTemplateLiterals": true } ],
+      "semi": [ "error", "always" ]
+  }
 };
--- rapla-parser.js
+++ rapla-parser.js
@@ -1,19 +1,18 @@
 const request = require('request');
-const jsdom = require("jsdom");
-const moment = require('moment');
-const Text = require("jsdom/lib/jsdom/living/generated/Text");
+const jsdom = require('jsdom');
+const Text = require('jsdom/lib/jsdom/living/generated/Text');
 const { JSDOM } = jsdom;
 
 function putProperty(tempEvent, values, label, i) {
   switch (label.innerHTML) {
-    case "Veranstaltungsname:":
-      tempEvent.title = values[i].innerHTML;
+  case 'Veranstaltungsname:':
+    tempEvent.title = values[i].innerHTML;
     break;
-    case "Personen:":
-      tempEvent.persons = values[i].innerHTML;
+  case 'Personen:':
+    tempEvent.persons = values[i].innerHTML;
     break;
-    case "Ressourcen:":
-      tempEvent.ressources = values[i].innerHTML;
+  case 'Ressourcen:':
+    tempEvent.ressources = values[i].innerHTML;
     break;
   }
 }
@@ -30,31 +29,34 @@ function applyMoment(srcMoment, time) {
 }
 
 function putDates(tempEvent, tempMoment, anchorElement) {
-  let time = "99:99-99:99";
+  let time = '99:99-99:99';
   const firstElNode = anchorElement.firstChild;
   if (typeof firstElNode === typeof Text) {
     time = firstElNode.textContent.substring(0,5).concat(firstElNode.textContent.substring(6));
   } else {
-    if (firstElNode.querySelectorAll("span.class=link")) {
+    if (firstElNode.querySelectorAll('span.class=link')) {
       putDates(firstElNode);
     } else {
-      time = "08:00-18:00";
+      time = '08:00-18:00';
     }
   }
+
+  console.log(`time: ${time}`);
+
   let times = time.split('-');
-  tempEvent.startDate = applyMoment(tempMoment, times[0]).format("HH:mm DD.MM.YYYY");
-  tempEvent.endDate = applyMoment(tempMoment, times[1]).format("HH:mm DD.MM.YYYY");
+  tempEvent.startDate = applyMoment(tempMoment, times[0]).format('HH:mm DD.MM.YYYY');
+  tempEvent.endDate = applyMoment(tempMoment, times[1]).format('HH:mm DD.MM.YYYY');
 }
 
 function addEvent(events, tempMoment, anchorElement) {
-  tempEvent = {};
+  let tempEvent = {};
 
   // Write the time
   putDates(tempEvent, tempMoment, anchorElement);
 
   // Write the details
-  let labels = anchorElement.querySelectorAll("span.tooltip table.infotable tbody tr td.label");
-  let values = anchorElement.querySelectorAll("span.tooltip table.infotable tbody tr td.value");
+  let labels = anchorElement.querySelectorAll('span.tooltip table.infotable tbody tr td.label');
+  let values = anchorElement.querySelectorAll('span.tooltip table.infotable tbody tr td.value');
   labels.forEach((label, i) => putProperty(tempEvent, values, label, i));
 
   events.push(tempEvent);
@@ -66,12 +68,12 @@ function fetchWeek(url, moment, onDone, onError, sharedObj) {
   request(`${url}&day=${moment.date()}&month=${moment.month()+1}&year=${moment.year()}`, (err, res, body) => {
     if (err && onError) return onError(err);
     const { document } = new JSDOM(body).window;
-    let weekTableRows = document.querySelectorAll("table.week_table > tbody > tr");
+    let weekTableRows = document.querySelectorAll('table.week_table > tbody > tr');
     weekTableRows.forEach(tr => {
       tr.childNodes.forEach(td => {
-        if (td.className === "week_block") {
-          addEvent(events, tempMoment, td.querySelector("a"));
-        } else if (td.className === "week_separatorcell") {
+        if (td.className === 'week_block') {
+          addEvent(events, tempMoment, td.querySelector('a'));
+        } else if (td.className === 'week_separatorcell') {
           tempMoment.add(1, 'days');
         }
       });
